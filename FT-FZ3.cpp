d90f9f6b237f7974fa88026e50c8b42651e0efbc
Wirx74 <162864606+Wirx74@users.noreply.github.com>
2024-04-26 14:19:51 +0500

    Create FT-FZ3.cpp

---
+++ FT-FZ3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    int* ptr = &arr[0];
+
+    std::cout << "The 4th element of the array: " << *(ptr + 3) << std::endl;
+
+    return 0;
+}
